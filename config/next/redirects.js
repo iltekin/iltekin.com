fff4c12e224c7d9dd158f0e57323609d2b920d87
Sezer Iltekin <sezer@iltekin.com>
2024-09-21 02:31:26 +0300

    Fix broken link

--- config/next/redirects.js
+++ config/next/redirects.js
@@ -31,7 +31,7 @@ const redirects = [
   buildRedirect('/dashsetup/:path*', 'https://dashbud.dev'),
   /* Other redirections */
   buildRedirect('/links', 'https://bio.jahir.dev'),
-  buildRedirect('/contact', 'https://hi.new/jahir'),
+  buildRedirect('/contact', 'https://hi.new'),
   /* Dashboard aliases */
   buildRedirect('/music', '/about#activity'),
   buildRedirect('/dashboard', '/about#activity'),
